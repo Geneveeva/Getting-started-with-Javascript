883ec721f4550654e5b9a443a4d65a8faa44defe
Geneveeva <lakegeneva85@gmail.com>
2023-02-23 11:23:09 +0100

    getting started with javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+function multiplyNumbers(x,y){
+        return x*y;
+    }
+
+let x = 4
+let y =8
+z= x*y
+
+        console.log(z);
+
+
+const myDetails = {
+    firstName:"Genevieve",
+    lastName: "Njoku",
+    stuternTrack: "front end developer",
+    skills: "HTML CSS Javascript",
+    favouriteColour : "Purple",
+    nigeriaIndependence : "1960",
+
+    fullDetails: function () {
+      return this.firstName + " " + this.lastName + " " + this.stuternTrack + " " + this.skills + " " + this.favouriteColour 
+      + " " + this.nigeriaIndependence;
+    }
+}
+    console.log(myDetails);
+  
